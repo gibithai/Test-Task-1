0a50d59b1d2e50e7124d7afd9c16bb52b77d12a3
gibithai <40383356+gibithai@users.noreply.github.com>
2019-05-28 00:17:56 +0300

    задание 3 подключить тестовый фреймворк и написать тест

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+
+import org.testng.annotations.Test;
+
+public class PointTests {
+    @Test
+
+    public void testArea() {
+        Point x, y = new Point(7, 9);
+        getClass();
+    }
+}
